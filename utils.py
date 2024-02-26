5bf80b6e2620bf5865a0793054654410977b5c62
suehi <110624980+007ekho@users.noreply.github.com>
2024-02-26 02:02:30 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -37,8 +37,8 @@ vectordb = Chroma(
 # chroma_retriever = Chroma()
 # retriever = vectordb.as_retriever(search_kwargs={"k": 2})
 from langchain.chat_models import ChatOpenAI
-from langchain import hub
-prompt = hub.pull("rlm/rag-prompt", api_url="https://api.hub.langchain.com")
+# from langchain import hub
+# prompt = hub.pull("rlm/rag-prompt", api_url="https://api.hub.langchain.com")
 llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613",openai_api_key= openai_api_key)
 
 from langchain.chains import RetrievalQA
